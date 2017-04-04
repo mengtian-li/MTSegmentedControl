039880cd6fccb5a03773214c932270e208b4ad2c
mengtian-li <mengtian.li@hotmail.com>
2017-04-04 18:38:15 +0800

    add Project files

---
+++ MTSegmentControlSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTSegmentControlSample
+//
+//  Created by LiMengtian on 2017/4/4.
+//  Copyright © 2017年 LiMengtian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTSegmentControlSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTSegmentControlSample
+//
+//  Created by LiMengtian on 2017/4/4.
+//  Copyright © 2017年 LiMengtian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
